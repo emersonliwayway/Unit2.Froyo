eedbcf63c0a7ba5344b4ff206a35139a3ce544c2
Emerson Recto <emersontrecto@gmail.com>
2025-03-24 20:27:37 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// user enters csv prompt
+// object created for each user prompt
+// input values are put into an array without the comma
+// key value pairs created for each unique flavor present
+// loop over input array to determine the amount of times the flavor is in the list
+// key values take the value of the amount
+// display table via console
+
+// user enters list
+// user's list stored in an array
+// object created for each user's input
+// array parsed into separate key values
+// array looped to count how many times each key is mentioned
+// object key assigned values based on result of previous loop
+// display user object as table
