84d4d2e49574bc9c5a87d0e56fa3ee09768cf4ab
Emerson Recto <emersontrecto@gmail.com>
2025-03-25 01:16:28 -0400

    finalized function addFlavor

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-// user enters csv prompt
-// object created for each user prompt
-// input values are put into an array without the comma
-// key value pairs created for each unique flavor present
-// loop over input array to determine the amount of times the flavor is in the list
-// key values take the value of the amount
-// display table via console
+// user enters string
+// parse string into an array
+// create blank object for user
+// loop through array and check if there is a matching key value in the object
+// if there isn't a matching key, add key to object and set value to 1
+// if there is a matching key, increase value by 1
+// display via console.table()
 
-// user enters list
-// user's list stored in an array
-// object created for each user's input
-// array parsed into separate key values
-// array looped to count how many times each key is mentioned
-// object key assigned values based on result of previous loop
-// display user object as table
+// prompt user and save input
+let userInput = prompt(
+  "Please enter a comma separated list of froyo flavors you'd like to order: ",
+  "vanilla, vanilla, chocolate, chocolate, mint"
+);
+
+// remove whitespaces and commas to make string an array, trim function doesn't work for this
+const userArray = userInput.split(", ");
+
+// function that creates a new object for each user, returns function with proper key values
+const addFlavor = (array) => {
+  const userObject = {};
+
+  // loop through list of flavors and add them to empty object on first iteration, after increase value by 1
+  for (i = 0; i < array.length; i++) {
+    if (array[i] in userObject == true) {
+      userObject[array[i]]++;
+    } else {
+      userObject[array[i]] = 1;
+    }
+  }
+  return userObject;
+};
+
+console.table(addFlavor(userArray));
